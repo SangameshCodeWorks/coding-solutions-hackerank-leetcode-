eb3776cc61d29ea81eef94cbf3834c0011ecd9db
Sangamesh Jainapur <sanguforwork123@gmail.com>
2026-09-23 22:28:10 +0530

    ✅ What's Your Name? | Medium | Python | Runtime: N/A | Memory: N/A | 2026-09-23

--- hackerrank/medium/whats-your-name/solution.py
+++ hackerrank/medium/whats-your-name/solution.py
@@ -1,6 +1,12 @@
-def split_and_join(line):
-    l = line.split()
-    l1 = "-".join(l)
-    return l1
-    # write your code here
 
+# Complete the 'print_full_name' function below.
+#
+# The function is expected to return a STRING.
+# The function accepts following parameters:
+#  1. STRING first
+#  2. STRING last
+#
+
+def print_full_name(first, last):
+    # Write your code here
+        print(f"Hello {first} {last}! You just delved into python.")
