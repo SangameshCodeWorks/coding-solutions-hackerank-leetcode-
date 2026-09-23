40ef6903bb6e30c205f11bd844f97bde09d6cb36
Sangamesh Jainapur <sanguforwork123@gmail.com>
2026-09-23 22:50:03 +0530

    ✅ Find a string | Medium | Python | Runtime: N/A | Memory: N/A | 2026-09-23

--- hackerrank/medium/find-a-string/solution.py
+++ hackerrank/medium/find-a-string/solution.py
@@ -1,6 +1,11 @@
-def mutate_string(string, position, character):
-    s = list(string)
-    s[position]=character
-    s_new = "".join(s)
-    return s_new
+def count_substring(string, sub_string):
+    count = 0
+    for i in range(0,len(string)):
+        s2=string[i:i+len(sub_string)]
+        if s2==sub_string:
+            count=count+1
+        else:
+            count=count+0
+    return count
+            
 
