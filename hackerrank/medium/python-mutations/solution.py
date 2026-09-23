f406961f45c5a209c40202c820b6d595b2bec2cb
Sangamesh Jainapur <sanguforwork123@gmail.com>
2026-09-23 22:39:53 +0530

    ✅ Mutations | Medium | Python | Runtime: N/A | Memory: N/A | 2026-09-23

--- hackerrank/medium/python-mutations/solution.py
+++ hackerrank/medium/python-mutations/solution.py
@@ -1,12 +1,6 @@
+def mutate_string(string, position, character):
+    s = list(string)
+    s[position]=character
+    s_new = "".join(s)
+    return s_new
 
-# Complete the 'print_full_name' function below.
-#
-# The function is expected to return a STRING.
-# The function accepts following parameters:
-#  1. STRING first
-#  2. STRING last
-#
-
-def print_full_name(first, last):
-    # Write your code here
-        print(f"Hello {first} {last}! You just delved into python.")
